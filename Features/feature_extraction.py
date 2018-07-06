e823ec7aa5d12c1711f069114d530aa13be75592
Rohith-Prem <rohithcnss@gmail.com>
2018-07-06 14:56:26 +0530

    Third Commit 6/07

--- Features/feature_extraction.py
+++ Features/feature_extraction.py
@@ -1,19 +1,22 @@
+# -*- coding=utf-8 -*-
 import re
+import csv
 
 in_file = open("tagged_split.txt", 'r', encoding='utf-8')
 number_of_words = open("wordcount.txt", 'r')
 text = in_file.read()
-#print(text)
+# print(text)
 words = text.split()
-#print(words)
+# print(words)
 tokens = [tk.split('\\') for tk in words if '\\' in tk]
-#print(tokens)
-tag = ['NNN', 'NNNP', 'NNST', 'VVMVNF', 'VVAUX'] #nnn-1 nnnp-2 nnnst-3 verb-4
+# print(tokens)
+tag = ['NNN', 'NNNP', 'NNST', 'VVMVNF', 'VVAUX']  # nnn-1 nnnp-2 nnnst-3 verb-4
 keywords = dict()
 id = 0
-#filtering nouns and verbs and assigning priority
+
+# filtering nouns and verbs and assigning priority
 for t in tokens:
-    if t[1] in tag and len(t[0])>2:
+    if t[1] in tag and len(t[0]) > 2:
         keywords.setdefault(id, []).append(t[0])
         if t[1] == "NNN":
             keywords.setdefault(id, []).append(1)
@@ -25,11 +28,12 @@ for t in tokens:
             keywords.setdefault(id, []).append(4)
         else:
             break
-            #keywords.setdefault(id, []).append(5)
+            # keywords.setdefault(id, []).append(5)
         id += 1
 
+# print(keywords)
 
-#counting frequency
+# counting frequency
 for i in keywords.keys():
     count = 0
     values = keywords[i]
@@ -40,27 +44,43 @@ for i in keywords.keys():
             count += 1
     keywords.setdefault(i, []).append(count)
 
-#print(keywords)
+# print(keywords)
 
-#deleting duplicates
+# deleting duplicates
 result = {}
 for key, value in keywords.items():
     if value not in result.values():
         result[key] = value
 
-#print(result)
+# print(result)
 
-#changing frequency to relative term frequency
+# changing frequency to relative term frequency
 wordcount = float(number_of_words.read())
 for key, value in result.items():
-    tf = value[2]/wordcount
+    tf = value[2] / wordcount
     result[key] = [value[0], value[1], tf]
 
-print(result)
+# print(result)
 
-#checking presence in heading and url
+# checking presence in heading and url
 headurl = open("E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Features\\head_url.txt", 'r', encoding='utf-8').read()
 headurl_words = headurl.split()
+# print(headurl_words)
 for key, value in result.items():
-    print(value[0])
+    if value[0] in headurl_words:
+        result.setdefault(key, []).append(1)
+    else:
+        result.setdefault(key, []).append(0)
+print(result)
+
+# writing to CSV file
+with open('features.csv', 'a') as csvfile:
+    fieldnames = ['POSTagPriority', 'TF', 'Head/URL']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for key,value in result.items():
+        #malword = str(value[0]).encode('utf-8')
+        writer.writerow({'POSTagPriority': value[1], 'TF': value[2], 'Head/URL': value[3]})
+
 
--- scrap.py
+++ scrap.py
@@ -15,8 +15,16 @@ import re
 
 from bs4 import BeautifulSoup
 
+
 def getWordsFromURL(url):
-	    return re.compile(r'[\:/?=\-&]+',re.UNICODE).split(url)
+    return re.compile(r'[\:/?=\-&]+', re.UNICODE).split(url)
+
+
+path = "E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\POSTagging"
+scrap = open('E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Tokenise\\scrapped_text.txt', 'a', encoding='utf-8')
+f = open("E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Features\\head_url.txt", 'a', encoding='utf-8')
+driver = webdriver.Firefox(path)
+driver.get('http://olam.in/Transliterate/')
 
 
 def getdata(i):
@@ -27,23 +35,19 @@ def getdata(i):
         links = soup.findAll("p")
         if len(links) != 1:
             head = soup.findAll("h1")[0].text
-            f = open("E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Features\\head_url.txt", 'a', encoding='utf-8')
             f.write(head)
-            txt = soup.findAll("h1")[0].text
-            txt += "\n"
-            #path = 'E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Tokenise'
+            txt=""
+            # path = 'E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Tokenise'
 
             for t in links:
                 txt += t.text
-            #filename = path + "\scrapped_text.txt"
-            f = open('E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Tokenise\\scrapped_text.txt', 'a', encoding='utf-8')
-            f.write(str(txt))
+            # filename = path + "\scrapped_text.txt"
+            scrap.write(str(txt))
     else:
         print("error")
 
 
 def main():
-    path = "E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\POSTagging"
     # print("enter the link\n")
     # ll=input()
     url1 = "http://www.mathrubhumi.com/news/kerala/malayalam/prof-t-j-joseph-abhimanyu-murder-maharajas-college-sfi-popular-front-sdpi-1.2945324"
@@ -53,22 +57,16 @@ def main():
     urls = getWordsFromURL(url1)
     # change this according to news url pattern
     urls = urls[2:len(urls) - 1]
-    f = open("E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Features\\head_url.txt", 'a', encoding='utf-8')
-    f.write("\n")
+    # f = open("E:\Work\ICFOSS\ICFOSS-KeyWord-Extractor\Features\\head_url.txt", 'a', encoding='utf-8')
+    # f.write("\n")
+    a = driver.find_element_by_id('transliterate')
     for u in urls:
         if len(u) > 3:
-	    
-		driver = webdriver.Firefox(path)
-		driver.get('http://olam.in/Transliterate/')
+            a.send_keys(u, Keys.ENTER)
+            sleep(3)
+    mal_u = a.get_attribute('value')
+    f.write(" " + mal_u)
 
-		a = driver.find_element_by_id('transliterate')
-		a.send_keys(u,Keys.ENTER)
-		sleep(2)
-		b = driver.find_element_by_id('transliterate')
 
-		mal_u = driver.find_element_by_id('transliterate').get_attribute('value')
-	        f.write(" " + mal_u)
-
-  
 if __name__ == "__main__":
     main()
